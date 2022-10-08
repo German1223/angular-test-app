6523c06afdaec2c5e41fc0d71fe82346315fe4ac
Herman1223 <germantv663@gmail.com>
2022-10-08 21:05:17 +0200

    Third commit

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,5 +1,5 @@
 import { APIService } from './../api_service/api.service';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -14,7 +14,6 @@ export class ConverterComponent {
     toExc: new FormControl(),
     firstSelect: new FormControl(),
   })
-
   valOne: number = 0;
   valTwo: number = 0;
   selOne: string = '';
@@ -22,17 +21,9 @@ export class ConverterComponent {
   USD: number = 0;
   EUR: number = 0;
   BTC: number = 0;
-  UAH: number = 0;
-  val1 = this.excForm.get('exchenge')
-  val2 = this.excForm.get('toExc')
-  select = this.excForm.get('firstSelect')
   constructor(private apiService: APIService) {
   }
   ngOnInit() {
-
-    this.excForm.valueChanges.subscribe((value) =>(this.valOne = value.exchange, console.log(value.exchange*1,value.firstSelect,value.toExc*1)));
-    
-
     this.apiService.getInfo().subscribe(data => {
       this.response = data
       this.USD = this.response[0].buy
@@ -40,36 +31,24 @@ export class ConverterComponent {
       this.BTC = this.response[2].buy
     })
   }
-  // setValue(one: number,two: number,three: number){
-  //   this.val1?.setValue(two)
-  //   this.val2?.setValue(one)
-  //   // this.select?.setValue(three)
-  // }
-  setVal(val: string) {
-    this.selOne = val
-  }
+  setVal() {
+    const toExcVal = this.excForm.get('exchange')!.value
+    const selVal = this.excForm.get('firstSelect')!.value
+    const secondVal = this.excForm.get('toExc')!.value
 
-  messageOne(one: number) {
-    console.log(this.selOne)
-    if (this.selOne === 'USD') {
-      this.valTwo = one / this.USD;
+    if (selVal === "USD") {
+      let res = toExcVal * this.USD;
+      this.excForm.patchValue({ toExc: res })
     }
-    if (this.selOne === 'EUR') {
-      this.valTwo = (one / this.EUR);
+
+    if (selVal === "EUR") {
+      let res = toExcVal * this.EUR;
+      this.excForm.patchValue({ toExc: res })
     }
-    if (this.selOne === 'BTC') {
-      this.valTwo = (one / this.BTC);
-    }
-  }
-  messageTwo(two: number) {
-    if (this.selOne === 'USD') {
-      this.valOne = two * this.USD;
-    }
-    if (this.selOne === 'EUR') {
-      this.valOne = two * this.EUR
-    }
-    if (this.selOne === 'BTC') {
-      this.valOne = two * this.BTC
+
+    if (selVal === "BTC") {
+      let res = toExcVal * this.BTC;
+      this.excForm.patchValue({ toExc: res })
     }
   }
 
